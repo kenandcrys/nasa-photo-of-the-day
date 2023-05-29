b2bdc1c33fc331f7882eb44bb1056634864974c6
Kenneth Fisher <kenandcrys@live.com>
2023-05-29 17:08:08 -0500

    Created Image and Title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Title from "./components/Title";
 import "./App.css";
+import Image from "./components/Image";
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
+    const cleanup = () => {
+      console.log('Performing cleanup...');
+    };
+    cleanup();
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Title title={data.title} />
+      <Image hdurl={data.hdurl} />
     </div>
   );
 }
---
+++ src/components/Image.js
@@ -0,0 +1,6 @@
+
+const Image = props => {
+    return <img src={props.hdurl} alt="NASA Photo" className="image" />;
+  };
+  
+  export default Image;
